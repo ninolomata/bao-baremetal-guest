a27f51200def49d50a8c18bfd292e3e88736988e
Bruno Sa <bruno.vilaca.sa@gmail.com>
2025-12-27 02:47:28 +0000

    fix abi
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>

--- src/arch/riscv/arch.mk
+++ src/arch/riscv/arch.mk
@@ -1,7 +1,7 @@
 CROSS_COMPILE ?= riscv64-unknown-elf-
 ARCH_SUB ?= riscv64xcheri
 ifeq ($(ARCH_SUB), riscv64xcheri)
-ARCH_GENERIC_FLAGS = -mcmodel=medany -march=rv64imafdxcheri -mabi=l64pc128 --target=riscv64 --sysroot=$(SYSROOT)/baremetal-newlib-riscv64-purecap/riscv64-unknown-elf
+ARCH_GENERIC_FLAGS = -mcmodel=medany -march=rv64imafdxcheri -mabi=l64pc128d --target=riscv64 --sysroot=$(SYSROOT)/baremetal-newlib-riscv64-purecap/riscv64-unknown-elf
 ARCH_ASFLAGS = --sysroot=$(SYSROOT)/baremetal-newlib-riscv64-purecap/riscv64-unknown-elf
 ARCH_LDFLAGS = --target=riscv64 --sysroot=$(SYSROOT)/baremetal-newlib-riscv64-purecap/riscv64-unknown-elf -lclang_rt.builtins-riscv64 -L$(SYSROOT)/baremetal-riscv64-purecap/lib
 else
